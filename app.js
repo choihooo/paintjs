d33a7e36aec2cb0e5e1a1eb8c2be546c24ed0411
choihooo <hochoi8621@gmail.com>
2021-07-24 23:37:52 +0900

    Size Range & Color Fill

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const canvas = document.getElementById("jsCanvas"); // jsCanvas를 가져와서 canvas에 집어넣는 거
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+
 let painting = false;
+let filling = false;
 
-canvas.width = 500;
-canvas.height = 500;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 500;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR; 
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 1.5;
-
 
 function stopPainting(){
     painting = false;
@@ -51,6 +58,30 @@ function handleColorClick(event){
     const bgColor = event.target.style.backgroundColor;
     // console.log(bgColor);
     ctx.strokeStyle = bgColor;
+    ctx.fillStyle = bgColor;
+}
+
+function handleRangeChange(event){
+    // console.log(event.target.value);
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "FILL"
+    }else{
+        filling = true
+        mode.innerText = "PAINT"
+        // ctx.fillStyle = ctx.strokeStyle;
+    }
+}
+
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+    }
 }
 
 if(canvas){
@@ -58,7 +89,16 @@ if(canvas){
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
 // console.log(Array.from(colors))
 Array.from(colors).forEach(color => color.addEventListener("click",  handleColorClick))
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
